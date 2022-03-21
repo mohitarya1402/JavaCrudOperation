e77c8231a9b022133b107e6a26923861b2424483
mohit <mohit@DESKTOP-M6TGBJ2>
2022-03-21 15:04:34 +0530

    CrudOperation

---
+++ src/main/java/crudoperation/model/User.java
@@ -0,0 +1,59 @@
+package crudoperation.model;
+
+public class User {
+    protected int id;
+    protected String name;
+    protected String email;
+    protected String country;
+    protected String city;
+
+    public User() {}
+
+    public User(String name, String email, String country, String city) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+    }
+
+    public User(int id, String name, String email, String country, String city) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
